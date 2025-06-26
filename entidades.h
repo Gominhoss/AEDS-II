40bb00f2ea66961117dbdb5363a375ae6d3720f9
Gominhoss <vitor.gferreira.08@gmail.com>
2025-06-26 16:10:05 -0300

    primeira versão feita pela procura-se

---
+++ entidades.h
@@ -0,0 +1,13 @@
+#ifndef ENTIDADES_H
+#define ENTIDADES_H
+
+#include "cliente.h"
+#include "estoque.h"
+#include "fornecedor.h"
+#include "funcionario.h"
+#include "produto.h"
+#include "utils.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#endif
